b29b03e8b0ef1aa41c358aa1823707a4686e2387
zelin-zx <1462285901@qq.com>
2018-09-03 18:51:15 +0800

    letao

---
+++ public/m/js/index.js
@@ -0,0 +1,27 @@
+$(function () {
+	var letao = new Letao();
+	//调用初始化轮播图的方法
+    letao.initSlide();
+    // 区域滚动的方法
+    letao.initScroll();
+})
+
+var Letao = function() {
+
+}
+
+Letao.prototype = {
+	//初始化轮播图的方法
+    initSlide: function() {
+        //获得slider插件对象
+        var gallery = mui('.mui-slider');
+        gallery.slider({
+            interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
+        });
+    },
+    initScroll:function(){
+        mui('.mui-scroll-wrapper').scroll({
+            deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+        });
+    }
+}
